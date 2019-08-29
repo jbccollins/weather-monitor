ad94a379bc0565b80631c9088cd3fb7b9762cddd
James Collins <jbccollins@gmail.com>
2019-08-29 09:59:14 -0400

    Use https for tiles

--- common/constants/urls.js
+++ common/constants/urls.js
@@ -1,7 +1,7 @@
 const RADAR_TIMESTAMPS = "https://tilecache.rainviewer.com/api/maps.json";
 const RADAR_TILES = "https://tilecache.rainviewer.com/v2/radar/{timestamp}/256/{z}/{x}/{y}.png";
 
-const IOWA_RADAR_TILES = "http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi";
+const IOWA_RADAR_TILES = "https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi";
 const WEATHER_FORECAST = "https://api.openweathermap.org/data/2.5/forecast?lat=38.889931&lon=-77.009003&units=imperial&appid=9962461c93445dbaf2529f24d498bab5&cnt=16";
 
 export {
